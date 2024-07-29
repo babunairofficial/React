69ee072106ffd0b4885dff6f8329983e6953081b
Babu Nair <babunairofficial@gmail.com>
2024-07-29 10:13:05 +0530

    understanding JSX

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+let name = "babu";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Babu
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos dolore, nesciunt amet ex in saepe deleniti cupiditate consequuntur repudiandae aut esse neque fugiat incidunt atque vel nihil corporis ut. Non repellat ex aliquid hic?
+      </p>
     </div>
+    </>
+    
   );
 }
 
