04701cb0a09b50b8c704931f7be0bb67215301f1
Babu Nair <babunairofficial@gmail.com>
2024-07-30 23:11:20 +0530

    adding more logic - words counter

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -17,19 +17,26 @@ export default function TextForm(props) {
   const [text, settext] = useState("Enter text here");
   const [result, setResult] = useState("");
   return (
-    <div>    
-        <h1>{props.heading}</h1>   
- 
-        <div className="mb-3">            
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-        </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
-        
-        <div className="mb-3 mt-3">
-          <h2>Result:</h2>          
-          <textarea className="form-control" value={result} readOnly id="resultBox" rows="8"></textarea>
-        </div>
-        
-    </div>    
+    <>
+      <div className="container">    
+          <h1>{props.heading}</h1>   
+  
+          <div className="mb-3">            
+              <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+          </div>
+          <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+          
+          <div className="mb-3 mt-3">
+            <h2>Result:</h2>          
+            <textarea className="form-control" value={result} readOnly id="resultBox" rows="8"></textarea>
+          </div>        
+      </div>
+
+      <div className="div container">
+        <h1>Your text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>  
+      </div>    
+
+    </>
   )
 }
