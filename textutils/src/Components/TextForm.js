77cc0c2fad77d14053fc3c639c89eec7a2a45059
Babu Nair <babunairofficial@gmail.com>
2024-07-30 13:16:57 +0530

    states and handling events-mychanges

--- textutils/src/Components/TextForm.js
+++ textutils/src/Components/TextForm.js
@@ -5,7 +5,8 @@ export default function TextForm(props) {
   const handleUpClick= ()=>{
     // console.log("UpperCase button was clicked" + text);
     let newText=text.toUpperCase();
-    settext(newText);
+    // settext(newText);
+    setResult(newText);
   }
 
   const handleOnChange= (event)=>{
@@ -14,13 +15,20 @@ export default function TextForm(props) {
   }
 
   const [text, settext] = useState("Enter text here");
+  const [result, setResult] = useState("");
   return (
     <div>    
-        <h1>{props.heading}</h1>    
+        <h1>{props.heading}</h1>   
+ 
         <div className="mb-3">            
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        
+        <div className="mb-3 mt-3">
+          <h2>Result:</h2>          
+          <textarea className="form-control" value={result} readOnly id="resultBox" rows="8"></textarea>
+        </div>
         
     </div>    
   )
