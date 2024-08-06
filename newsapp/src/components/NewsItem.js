eeea1f4165dab2a32e70807b2fc158b3c7834bc1
Babu Nair <babunairofficial@gmail.com>
2024-08-06 19:55:05 +0530

    fetch newsapi

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>        
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://i-invdn-com.investing.com/news/Brazil-Stock-Market_3_800x533_L_1414427437.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
